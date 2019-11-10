35389b986c46d4680e534d5bbd8356b927d064d7
unknown <lena.pohlmann@rwth-aachen.de>
2019-11-10 17:09:49 +0100

    Erstellug AnalysisScript.R

---
+++ AnalysisScript.R
@@ -0,0 +1,10 @@
+# AnalysisScript.R
+## Teamnamen: Transformation and Leadership
+
+# Pakete laden: ----
+
+# Rohdaten laden: ----
+
+# DataCleaning und Skalenberechnung: ----
+
+# Statistische Analyse und Grafiken: ----
